499507004711153ff113aaf91e43d94677b5aee4
Christine <christineitrinh@gmail.com>
2019-06-06 10:20:42 -0500

    trying to add methods to replace text once click event is triggered

--- app.js
+++ app.js
@@ -7,30 +7,36 @@ function randomValueFromArray(array) {
     return array[Math.floor(Math.random() * array.length)];
 }
 
-let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.'
+var storyText = 'It was 94 fahrenheit outside, so' + xItem + 'went for a walk. When they got to' + yItem + ', they stared in horror for a few moments, then' + zItem + '. Bob saw the whole thing, but was not surprised — ' + xItem + 'weighs 300 pounds, and it was a hot day.'
 
 // Raw text strings
-let insertX = 'Willy the Goblin, Big Daddy, Father Christmas'
-let insertY = 'the soup kitchen, Disneyland, the White House'
-let insertZ = 'spontaneously combusted, melted into a puddle on the sidewalk, turned into a slug and crawled away'
+let insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
+let insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
+let insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
 
 
 // Event Listener
 randomize.addEventListener('click', result);
 
 function result() {
+    let newStory = storyText;
+    var xItem = randomValueFromArray(insertX).replace(xItem, insertX);
+    var yItem = randomValueFromArray(insertY).replace(yItem, insertY);
+    var zItem = randomValueFromArray(insertZ).replace(zItem, insertZ);
 
     if (customName.value !== '') {
         var name = customName.value;
+        var newName = storyText.replace('Bob', name)
 
     }
 
     if (document.getElementById("uk").checked) {
-        var weight = Math.round(300);
-        var temperature = Math.round(94);
-
+        var weight = (Math.round(300 * 0.071429) + ' stone');
+        var temperature = (Math.round((94 - 32) * (5 / 9)) + ' centigrade');
+        var newWeight = storyText.replace('300 pounds', weight);
+        var newTemp = storyText.replace('94 fahrenheit', temperature)
     }
 
-    story.textContent = ;
+    story.textContent = newStory;
     story.style.visibility = 'visible';
 }
