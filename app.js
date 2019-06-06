c5d3038c9ffbfa84081532421c0e2e10e6220ea7
Christine <christineitrinh@gmail.com>
2019-06-06 10:49:21 -0500

    changed all variable declarations to 'let'

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ function randomValueFromArray(array) {
     return array[Math.floor(Math.random() * array.length)];
 }
 
-var storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.'
+let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.'
 
 // Raw text strings
 let insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
@@ -22,23 +22,23 @@ function result() {
 
 
     let newStory = storyText;
-    var xItem = randomValueFromArray(insertX);
-    var yItem = randomValueFromArray(insertY);
-    var zItem = randomValueFromArray(insertZ);
+    let xItem = randomValueFromArray(insertX);
+    let yItem = randomValueFromArray(insertY);
+    let zItem = randomValueFromArray(insertZ);
     newStory = newStory.replace(':insertx:', xItem);
     newStory = newStory.replace(':inserty:', yItem)
     newStory = newStory.replace(':insertz:', zItem)
     newStory = newStory.replace(':insertx:', xItem)
 
     if (customName.value !== '') {
-        var name = customName.value;
+        let name = customName.value;
         newStory = newStory.replace('Bob', name)
 
     }
 
     if (document.getElementById("uk").checked) {
-        var weight = Math.round(300 * 0.071429) + ' stone';
-        var temperature = Math.round((94 - 32) * (5 / 9)) + ' centigrade';
+        let weight = Math.round(300 * 0.071429) + ' stone';
+        let temperature = Math.round((94 - 32) * (5 / 9)) + ' centigrade';
         newStory = newStory.replace('94 fahrenheit', temperature);
         newStory = newStory.replace('300 pounds', weight);
     }
